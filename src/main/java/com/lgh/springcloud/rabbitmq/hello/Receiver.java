8964211b79336d4e0c2c96b26616d6e3e36cf4dd
Johnson Lau <gatement@gmail.com>
2018-04-15 12:30:07 +0800

    go

--- src/main/java/com/lgh/springcloud/rabbitmq/hello/Receiver.java
+++ src/main/java/com/lgh/springcloud/rabbitmq/hello/Receiver.java
@@ -1,5 +1,8 @@
 package com.lgh.springcloud.rabbitmq.hello;
 
+import java.io.UnsupportedEncodingException;
+
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -7,13 +10,13 @@ import org.springframework.stereotype.Component;
 import lombok.extern.slf4j.Slf4j;
 
 @Component
-@RabbitListener(queues="hello")
+@RabbitListener(queues = "hello")
 @Slf4j
 public class Receiver {
 
 	@RabbitHandler
-	public void process(String hello) {
-		log.info("Receiver: " + hello);
+	public void process(Object msg) throws UnsupportedEncodingException {
+		Message msg2 = (Message) msg;
+		log.info("Receiver========: " + new String(msg2.getBody(), "UTF-8"));
 	}
-
 }
